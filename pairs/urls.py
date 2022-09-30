832ac9b167cfc87d5e9d9997cb94084c6edd5e03
tnpfldyd <dkdldhs0322@gmail.com>
2022-09-30 12:55:23 +0900

    0930

--- pairs/urls.py
+++ pairs/urls.py
@@ -5,5 +5,10 @@ app_name = 'pairs'
 
 urlpatterns = [
     path('index/<int:num>', views.index, name='index'),
-
+    path('create/', views.create, name='create'),
+    path('create2/', views.create2, name='create2'),
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('edit2/<int:pk>', views.edit2, name='edit2'),
 ]
--- pairs/views.py
+++ pairs/views.py
@@ -1,14 +1,10 @@
-from multiprocessing import context
-from turtle import title
-from unittest import result
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
 import math
 
 # Create your views here.
 def index(request, num):
-
-    title = Articles.objects.order_by('updated_at')[(num-1)*15:num*15]
+    title = Articles.objects.order_by('-updated_at')[(num-1)*15:num*15]
     btn = math.ceil(Articles.objects.all().count()/15)
     result = []
     for i in range(1, btn + 1):
@@ -17,6 +13,33 @@ def index(request, num):
         'titles':title,
         'numbers':result,
     }
-
     return render(request, 'pairs/index.html', context)
-
+def create(request):
+    return render(request, 'pairs/create.html')
+def create2(request):
+    info = request.GET
+    Articles.objects.create(title=info['title'], content=info['content'], user_name=info['name'], password=info['password'])
+    return redirect('pairs:index', 1)
+def detail(request, pk):
+    id = Articles.objects.get(pk=pk)
+    context = {
+        'id': id,
+    }
+    return render(request, 'pairs/detail.html', context)
+def edit(request, pk):
+    id = Articles.objects.get(pk=pk)
+    context = {
+        'id': id,
+    }
+    return render(request, 'pairs/edit.html', context)
+def edit2(request, pk):
+    id = Articles.objects.get(pk=pk)
+    edit = request.GET
+    id.title = edit['title']
+    id.content = edit['content']
+    id.save()
+    return redirect('pairs:index', 1)
+def delete(request, pk):
+    id = Articles.objects.get(pk=pk)
+    id.delete()
+    return redirect('pairs:index', 1)
