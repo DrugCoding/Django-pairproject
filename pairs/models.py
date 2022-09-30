a490e47659b2e4c00b97b62890d1be4295fc4c2a
tnpfldyd <dkdldhs0322@gmail.com>
2022-09-30 10:05:47 +0900

    0930

--- pairs/models.py
+++ pairs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length = 80)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user_name = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 80)
